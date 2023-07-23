827a4b7e0f77b00d6e8157af5af7c7f02152b52c
AVINASH SINGH <avisinghone@gmail.com>
2023-07-23 13:58:38 +0530

    import & load routes in server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
 
+// import routes
+import web from './routes/web.js'
+
 const server=express();
 const port= process.env.PORT || '4000'
 
 // Cookie Parser middleware
 server.use(cookieParser());
+
+// Load routes
+server.use('/',web);
 
 server.listen(port,()=>{
     console.log(`Server listening at http://localhost:${port}`)
